88e2bdd0a84f49b66e8c10bfc6589b60f4877fb5
Daniel Schneider <daniel.schneider@elasticpath.com>
2024-09-05 15:21:19 -0400

    update songs and bug fix

--- app/components/Dashboard/Cards/ArtistCard.tsx
+++ app/components/Dashboard/Cards/ArtistCard.tsx
@@ -124,7 +124,7 @@ export default function ArtistCard() {
                 <label htmlFor="floating_all_of_artist" className="cursor-pointer peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 ml-3">Add all songs by artist</label>
               </div>
               <TypeList setList={setArtists} list={generalInfoData.artists} className={"pl-4 pr-4"}/>
-              <button disabled={artist === undefined || !artist.label} onClick={() => addArtistSongs()} id="button" type="button" className="disabled:bg-indigo-800 bg-indigo-600 shadow-xl hover:bg-indigo-500 text-white font-bold rounded-full p-4 w-48 mx-auto">Add Songs</button>
+              <button disabled={!artist || !artist.label} onClick={() => addArtistSongs()} id="button" type="button" className="disabled:bg-indigo-800 bg-indigo-600 shadow-xl hover:bg-indigo-500 text-white font-bold rounded-full p-4 w-48 mx-auto">Add Songs</button>
           </div>
           {/* <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button> */}
       </div>
