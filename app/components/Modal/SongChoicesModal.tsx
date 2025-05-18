ca42d40f1d5612f6f913eb57e5c29ff44100db6d
Daniel Schneider <dschneider@bandwidth.com>
2025-05-18 19:13:30 -0400

    test rabbit

--- app/components/Modal/SongChoicesModal.tsx
+++ app/components/Modal/SongChoicesModal.tsx
@@ -62,7 +62,7 @@ export default function SongChoicesModal({
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                Multiple Song Choices Found: Double Click to Select One
+                Multiple Song Choices Found -- Double Click to Select One
               </h3>
               <button
                 onClick={() => ignoreChoice()}
