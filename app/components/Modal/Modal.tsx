a51da239877355d869f689ec54274f7e8d3bab8b
Daniel Schneider <dschneider@bandwidth.com>
2025-05-18 20:18:51 -0400

    suggested fixes

--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -8,7 +8,6 @@ interface Props {
   placeholder: string;
   closeModal: () => void;
 }
-// I'm a dumb comment that doesn't add anything of relevance
 
 export default function Modal({
   header,
@@ -62,7 +61,7 @@ export default function Modal({
                 onChange={(e) => setText(e.currentTarget.value)}
                 placeholder={placeholder}
                 className="placeholder-slate-400 p-1 min-h-300 outline-none focus:ring focus:border-slate-300 dark:bg-gray-600 text-base leading-relaxed text-gray-200 dark:text-gray-200 w-full"
-              ></textarea>
+              />
             </div>
             <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
               <button
