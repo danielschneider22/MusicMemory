b3e6af9b3d2b9964bf717bdbb35c40c24e016299
danielschneider22 <danielschneider22@gmail.com>
2025-12-20 09:53:21 -0500

    fix css issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import './ag-grid/ag-grid.css'
-import './ag-grid/ag-theme-alpine.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import AuthProvider from './auth/Provider'
-import NavBar from './NavBar'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import AuthProvider from "./auth/Provider";
+import NavBar from "./NavBar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Music Memories',
-  description: 'Enter user details to get a list of songs curated to your interests.',
-}
+  title: "Music Memories",
+  description:
+    "Enter user details to get a list of songs curated to your interests.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className="dark">
@@ -30,7 +31,6 @@ export default function RootLayout({
           {children}
         </body>
       </AuthProvider>
-      
     </html>
-  )
+  );
 }
